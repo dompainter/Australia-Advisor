b75997d563ffcfb62d9b0b4adf83b474605b1311
Dom Painter <dominickpainter@gmail.com>
2019-08-25 15:32:55 +1000

    Add container and layout structure

--- src/index.js
+++ src/index.js
@@ -1,12 +1,37 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import styled, { createGlobalStyle } from 'styled-components'
+
 import Header from './components/header'
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        background-color: #EEEFF3;
+        padding: 20px 40px;
+        font-family: Arial, Helvetica, sans-serif;
+    }
+`
+
+const AppContainer = styled.main`
+    width: 100%;
+    height: 100%;
+`
+
+const ContentContainer = styled.main`
+    background-color: #FFF;
+    padding: 15px;
+`
 
 const App = () => {
     return (
-        <div>
+        <AppContainer>
             <Header title="Australia-Advisor"/>
-        </div>
+            <ContentContainer>
+                <span>Content goes here</span>
+            </ContentContainer>
+            <GlobalStyle />
+        </AppContainer>
     )
 }
 
